8073ec4349e5624903cb1a51661a7771e6333dbf
Manish Nakar <manish.nakar@gmail.com>
2021-04-25 00:09:40 +0530

    initial setup

---
+++ server/config/mongodb.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import mongoose from 'mongoose'
+
+const db = process.env.MONGO_URI
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    // console.log('Mongodb connected')
+  } catch (error) {
+    // console.error(error.message)
+    process.exit(1)
+  }
+}
+
+export default connectDB
---
+++ server/routes/index.js
@@ -0,0 +1,42 @@
+'use strict'
+
+import express from 'express'
+
+const router = express.Router()
+
+router.get('/health-status', (req, res, next) => {
+  res.status(200).json('ok')
+})
+
+router.get('/', (req, res) => {
+  res.send('ok')
+})
+
+
+// error handeling
+router.use('*', (req, res, next) => {
+  const error = {
+    statusCode: 404,
+    message: ['Cannot', req.method, req.originalUrl].join(' ')
+  }
+  next(error)
+})
+
+router.use((error, req, res, next) => {
+  if (!error) {
+    return
+  }
+  const isParseError = error instanceof SyntaxError && error.status === 400
+  if (isParseError) {
+    return res.status(400).json('Invalid JSON body')
+  }
+  if (error.statusCode) {
+    if (error.statusCode === 404) {
+      return res.status(404).send('File not found')
+    }
+    return res.status(error.statusCode).json(error)
+  }
+  return res.status(500).json(error)
+})
+
+export { router }
